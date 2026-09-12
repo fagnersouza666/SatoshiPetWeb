6a18149c141bb41eb4260353020fbd3acc133fa2
fury <fagnersouza@gmail.com>
2026-09-12 16:30:31 +0000

    test: ajustar contrato de redaction dos eventos (spw-1qo.16.1.3)

--- services/api/src/test/java/br/com/satoshipet/api/events/BitcoinEventRedactionContractTest.java
+++ services/api/src/test/java/br/com/satoshipet/api/events/BitcoinEventRedactionContractTest.java
@@ -7,8 +7,8 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Iterator;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
